1d2d4805cb802bceff01bc61dd7bf1992ffa63c5
yishain11 <yishain11@walla.co.il>
2024-03-18 21:51:52 +0200

    added writing functionality with rand err

--- src/main.py
+++ src/main.py
@@ -6,5 +6,9 @@ if len(sys.argv)>1:
     match operation:
         case 'read':
             fns.read_from_json()
+        case 'write':
+            msg = input("what is your msg? ")
+            if msg:
+                fns.write_to_json(msg)
 else:
     print("no operator")
--- src/utils/json_fns.py
+++ src/utils/json_fns.py
@@ -1,13 +1,18 @@
-import random
+import json
 from utils.random_err import gen_rand_err
 
 def write_to_json(msg):
-    gen_rand_err()
-    pass
-
+    with open('./data/journal.json','r+') as f:
+        f.seek(0)
+        content = f.read()
+        content = json.loads(content)
+        content.append(msg)
+        f.seek(0)        
+        json.dump(content,f)
+        f.truncate()
 def read_from_json():
     # read from json, but fail 50% of the time
-    gen_rand_err()
     with open('./data/journal.json') as f:
+        gen_rand_err()
         print(f.read())
         return 
